54c6e2a940c6017f5f80a110a1bf49a3d3e48cc3
claire-brilleaud <26025@student.dorset-college.ie>
2024-01-22 18:47:13 +0100

    Troisieme Version

--- src/main/java/com/example/claireb/ComponentActivity.kt
+++ src/main/java/com/example/claireb/ComponentActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -38,6 +41,7 @@ class BengaluruActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     GreetingText(Weather("19°","Bengaluru,India","Mid Rain","H:24° L:18"))
+                    ImageComponent("moon_cloud_mid_rain")
                 }
             }
         }
@@ -52,77 +56,121 @@ val sfProDisplayFamily = FontFamily(
 data class Weather(val temperature: String, val localisation: String, val type: String, val range: String)
 
 @Composable
+fun ImageComponent(image: String, modifier: Modifier = Modifier){
+    if(image=="moon_cloud_mid_rain"){
+        Image(
+            painter = painterResource(R.drawable.moon_cloud_mid_rain),
+            contentDescription = null,
+            modifier = Modifier.size(160.dp)
+                .offset(x=178.dp,y=-40.dp)
+                )
+    }else if(image=="moon_fast_wind"){
+        Image(
+            painter = painterResource(R.drawable.moon_fast_wind),
+            contentDescription = null,
+            modifier = Modifier
+                .size(160.dp)
+                .offset(x = 178.dp, y = -40.dp))
+    }else if(image=="cloud_zap"){
+        Image(
+            painter = painterResource(R.drawable.cloud_zap),
+            contentDescription = null,
+            modifier = Modifier
+                .size(160.dp)
+                .offset(x = 178.dp, y = -40.dp))
+    }else{
+        Image(
+            painter = painterResource(R.drawable.sun_cloud_angled_rain),
+            contentDescription = null,
+            modifier = Modifier
+                .size(160.dp)
+                .offset(x = 178.dp, y = -40.dp))
+    }
+}
+@Composable
 fun GreetingText(name: Weather, modifier: Modifier = Modifier) {
     //Spacer(modifier = Modifier.height(30.dp))
-    Image(
-        painter = painterResource(R.drawable.rectangle),
-        contentDescription = null,
-        //modifier = Modifier.size(333.dp)
-        modifier = Modifier.width(338.dp)
-            .height(185.dp)
+    Box{
+        Image(
+            painter = painterResource(R.drawable.rectangle),
+            contentDescription = null,
+            //modifier = Modifier.size(333.dp)
+            modifier = Modifier
+                .width(338.dp)
+                .height(185.dp)
 
-
-    )
-    Column (modifier = Modifier.padding(20.dp)){
-        //Spacer(modifier = Modifier.height(8.dp))
-        //Spacer(modifier = Modifier.width(20.dp))
-        Text(
-            text = name.temperature,
-            //color = MaterialTheme.colorScheme.primary,
-            color = Color.White,
-            //style = MaterialTheme.typography.bodyLarge
-            fontFamily = sfProDisplayFamily,
-            //fontWeight = FontWeight.Normal,
-            fontSize = 64.sp,
-            lineHeight = 70.sp,
-            letterSpacing = 0.37.sp
-            )
-        Spacer(modifier = Modifier.height(20.dp))
-        Text(
-            text = name.range,
-            //color = MaterialTheme.colorScheme.primary,
-            color = MaterialTheme.colorScheme.surface.copy(alpha = 0.6f),
-            //style = MaterialTheme.typography.labelSmall
-            fontFamily = sfProDisplayFamily,
-            //fontWeight = FontWeight.Medium,
-
-            fontSize = 13.sp,
-            lineHeight = 18.sp,
-            letterSpacing = -0.08.sp
         )
-        Row{
+        Column (modifier = Modifier.padding(20.dp)){
+            //Spacer(modifier = Modifier.height(8.dp))
+            //Spacer(modifier = Modifier.width(20.dp))
+
             Text(
-                text = name.localisation,
+                text = name.temperature,
                 //color = MaterialTheme.colorScheme.primary,
                 color = Color.White,
-                //style = MaterialTheme.typography.titleLarge
+                //style = MaterialTheme.typography.bodyLarge
                 fontFamily = sfProDisplayFamily,
                 //fontWeight = FontWeight.Normal,
-                fontSize = 17.sp,
-                lineHeight = 22.sp,
-                letterSpacing = -0.41.sp
+                fontSize = 64.sp,
+                lineHeight = 70.sp,
+                letterSpacing = 0.37.sp
             )
-            Spacer(modifier = Modifier.width(151.dp))
+            Spacer(modifier = Modifier.height(20.dp))
             Text(
-                text = name.type,
+                text = name.range,
                 //color = MaterialTheme.colorScheme.primary,
-                color = Color.White,
-                //style = MaterialTheme.typography.titleLarge
+                color = MaterialTheme.colorScheme.surface.copy(alpha = 0.6f),
+                //style = MaterialTheme.typography.labelSmall
                 fontFamily = sfProDisplayFamily,
-                //fontWeight = FontWeight.Normal,
+                //fontWeight = FontWeight.Medium,
+
                 fontSize = 13.sp,
                 lineHeight = 18.sp,
                 letterSpacing = -0.08.sp
             )
+            Row{
+                Text(
+                    text = name.localisation,
+                    //color = MaterialTheme.colorScheme.primary,
+                    color = Color.White,
+                    //style = MaterialTheme.typography.titleLarge
+                    fontFamily = sfProDisplayFamily,
+                    //fontWeight = FontWeight.Normal,
+                    fontSize = 17.sp,
+                    lineHeight = 22.sp,
+                    letterSpacing = -0.41.sp
+                )
+                Text(
+                    text = name.type,
+                    //color = MaterialTheme.colorScheme.primary,
+                    color = Color.White,
+                    //style = MaterialTheme.typography.titleLarge
+                    fontFamily = sfProDisplayFamily,
+                    //fontWeight = FontWeight.Normal,
+                    fontSize = 13.sp,
+                    lineHeight = 18.sp,
+                    letterSpacing = -0.08.sp,
+                    modifier = Modifier.offset(x=151.dp,y=5.dp)
+                )
+            }
         }
     }
+
 }
 
+@Composable
+fun Components(name:Weather, image:String, modifier : Modifier=Modifier){
+    Box{
+        GreetingText(name)
+        
+        ImageComponent(image = image)
+    }
 
+}
 @Preview(showBackground = false)
 @Composable
 fun GreetingPreview2() {
     ClaireBTheme {
-        GreetingText(Weather("19°","Bengaluru,India","Mid Rain","H:24° L:18"))
+        Components(name = Weather("19°","Bengaluru,India","Mid Rain","H:24° L:18"), image = "moon_cloud_mid_rain")
     }
 }
--- src/main/java/com/example/claireb/MainActivity.kt
+++ src/main/java/com/example/claireb/MainActivity.kt
@@ -3,13 +3,30 @@ package com.example.claireb
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.claireb.ui.theme.ClaireBTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +39,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    //Components(name = Weather("19°","Bengaluru,India","Mid Rain","H:24° L:18"), image = "moon_cloud_mid_rain")
+                    //Components(name = Weather("19°","Bengaluru,India","Mid Rain","H:24° L:18"), image = "moon_cloud_mid_rain")
+                    Components2()
                 }
             }
         }
@@ -37,10 +57,76 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Components2(modifier : Modifier=Modifier){
+    LazyColumn(horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .verticalScroll(
+                rememberScrollState()
+            )
+    ) {
+        item{
+            Column (modifier = Modifier
+                .fillMaxWidth()
+                .padding(25.dp)){
+                Row(modifier = Modifier
+                    .fillMaxWidth(),
+                    verticalAlignment = Alignment.Top,
+                    horizontalArrangement  =  Arrangement.Absolute.SpaceBetween){
+                    Text(
+                        text = "Weather",
+                        color = Color.White,
+                        //style = MaterialTheme.typography.bodyLarge
+                        fontFamily = sfProDisplayFamily,
+                        //fontWeight = FontWeight.Normal,
+                        fontSize = 28.sp,
+                        lineHeight = 34.sp,
+                        letterSpacing = 0.36.sp)
+                    Spacer(modifier = Modifier.height(50.dp))
+                    Image(
+                        painter = painterResource(R.drawable.right_title),
+                        contentDescription = null,
+                        modifier= Modifier
+                            .width(33.dp)
+                            .height(34.dp))
+
+                }
+                Image(
+                    painter = painterResource(R.drawable.search_field),
+                    contentDescription = null,
+                    modifier= Modifier
+                        .width(358.dp)
+                        .height(36.dp))
+            }
+        }
+        item {
+            Components(
+                name = Weather("19°", "Bengaluru,India", "Mid Rain", "H:24° L:18"),
+                image = "moon_cloud_mid_rain"
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+        }
+        item{
+            Components(name = Weather("22°","Chennai,India","Fast Wind","H:26° L:18"), image = "moon_fast_wind")
+            Spacer(modifier = Modifier.height(30.dp))
+        }
+        item{
+            Components(name = Weather("29°","Delhi,India","Cloudy","H:32° L:27"), image = "sun_cloud_angled_rain")
+            Spacer(modifier = Modifier.height(30.dp))
+        }
+        item{
+            Components(name = Weather("25°","Mumbai,India","Stormy","H:32° L:20"), image = "cloud_zap")
+        }
+    }
+}
+
+@Preview(showBackground = false)
 @Composable
 fun GreetingPreview() {
     ClaireBTheme {
-        Greeting("Android")
+        Components2()
+
+
     }
 }
